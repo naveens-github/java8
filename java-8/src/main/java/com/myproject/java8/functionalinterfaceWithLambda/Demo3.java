a0748bdb5e0188d82aa871fc90ab49b3ac4c5095
naveen <naveenwork8@gmail.com>
2023-03-30 12:38:15 -0500

    java 8 features lambda expression and functional interface example 3

--- java-8/src/main/java/com/myproject/java8/functionalinterfaceWithLambda/Demo3.java
+++ java-8/src/main/java/com/myproject/java8/functionalinterfaceWithLambda/Demo3.java
@@ -4,5 +4,6 @@ public class Demo3 {
     public static void main(String[] args) {
         Interf3 i = s -> s.length();
         System.out.println(i.getLength("Naveen"));
+        System.out.println(i.getLength("Naveen    "));//Count spaces too
     }
 }
