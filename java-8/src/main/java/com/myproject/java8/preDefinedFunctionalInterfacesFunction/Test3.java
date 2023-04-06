13ad9d80e1d35adf57086b0ef14ea733a9b069b5
naveen <naveenwork8@gmail.com>
2023-04-06 17:27:32 -0500

    Function functional interface with different examples

---
+++ java-8/src/main/java/com/myproject/java8/preDefinedFunctionalInterfacesFunction/Test3.java
@@ -0,0 +1,38 @@
+package com.myproject.java8.preDefinedFunctionalInterfacesFunction;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+class Employee{
+    String name;
+    double salary;
+    Employee(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+    }
+    public String toString(){
+        return name+":"+salary;
+    }
+}
+
+public class Test3 {
+    public static void main(String[] args) {
+        ArrayList<Employee> list = new ArrayList<>();
+        populate(list);
+        Function<ArrayList<Employee>, Double> f1 = l-> {
+            double total = 0;
+            for(Employee e1: l)
+            total = total + e1.salary;
+            return total;
+        };
+        System.out.println("total salary is: "+f1.apply(list));
+    }
+    public static void populate(ArrayList<Employee> list){
+        list.add(new Employee("Mahomes", 12000));
+        list.add(new Employee("Tyreek", 5000));
+        list.add(new Employee("Kelce", 11000));
+        list.add(new Employee("Chris", 7000));
+        list.add(new Employee("Micheal", 3500));
+
+    }
+}
