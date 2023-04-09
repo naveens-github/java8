93c70551010f6acceb5023b546ee34b6e26ef2cc
naveen <naveenwork8@gmail.com>
2023-04-09 13:41:48 -0500

    primitive type functional interface with different examples

---
+++ java-8/src/main/java/com/myproject/java8/primitiveTypeFunctionalInterfaces/Test1IntPredicate.java
@@ -0,0 +1,14 @@
+package com.myproject.java8.primitiveTypeFunctionalInterfaces;
+
+import java.util.function.IntPredicate;
+
+public class Test1IntPredicate {
+    public static void main(String[] args) {
+        int[] x ={1, 2, 3, 10, 20, 5, 17};
+        IntPredicate p =i->i%2==0;
+        for(int x1 :x) {
+            if(p.test(x1))
+                System.out.println("even numbers are: "+x1);
+        }
+    }
+}
